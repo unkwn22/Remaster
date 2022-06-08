ad1f9d9dd2e2e8382e3150090b5fd3e38933446d
unkwn <lsjc12911@naver.com>
2022-06-08 12:06:17 +0900

    TODO

--- src/MAIN/Game.java
+++ src/MAIN/Game.java
@@ -1,7 +1,25 @@
 package MAIN;
 
+import INIT.*;
+
+import java.util.Scanner;
+
 public class Game {
 
+    Scanner scan = new Scanner(System.in);
 
+    public Game(){
+        InitObject init = new InitObject();
+        // TODO will i have to pass the instance to another package or make its own handler
+        // TODO making its own handler page inside of the INVENTORY package or on its own
+        while(true){
+            int num = scan.nextInt();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        new Game();
+    }
 
 }
--- src/MAIN/GamePanel.java
+++ src/MAIN/GamePanel.java
@@ -1,4 +1,8 @@
 package MAIN;
 
+import java.util.HashMap;
+
 public class GamePanel {
+
+    static HashMap<Object, String> NUM = new HashMap<>();
 }

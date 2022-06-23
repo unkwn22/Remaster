a9998d741fa1bc88cb3afebbd6b0768feafe3d9b
unkwn <lsjc12911@naver.com>
2022-06-23 15:13:41 +0900

    created a container and a jPanel

--- src/MAIN/Game.java
+++ src/MAIN/Game.java
@@ -8,13 +8,25 @@ import java.awt.*;
 
 public class Game {
 
+    // creating a basic frame
     JFrame window;
+    // a container that the window will carry
+    Container container;
 
-    // Frame properties
-    int windowWidth = 800;
-    int windowHeight = 600;
-    Color backGroundColor = Color.black;
-    boolean visibleSet = true;
+    // window properties
+    int windowWidth       = 800;
+    int windowHeight      = 600;
+    Color frameColor = Color.black;
+    boolean visibleSet    = true;
+
+    // a component for the title
+    JPanel titleNamePanel;
+    // titleNamePanel properties
+    int boundX                = 90;
+    int boundY                = 100;
+    int boundWidth            = 600;
+    int boundHeight           = 150;
+    Color titleNamePanelColor = Color.blue;
 
     public Game(){
 
@@ -25,11 +37,26 @@ public class Game {
         // creating a JFrame close option, used to make the JFrame close properly
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // setting the frame background color
-        window.getContentPane().setBackground(backGroundColor);
+        window.getContentPane().setBackground(frameColor);
         // setting a the frame layout, setting it to null means turning off the default layout
         window.setLayout(null);
         // even though we created a window component, it isnt visible in the UI so to make it visible...
         window.setVisible(visibleSet);
+
+        // when wanting to create a new components on top of the JFrame, the JFrame must have a container
+        // so were letting the container know that were going to make the JFrame as a base layout component
+        // so now letting the container constructor know that the window Content pane is going to be a new Container object
+        container = window.getContentPane();
+
+        // creating a title name panel
+        titleNamePanel = new JPanel();
+        // setting the panel bounds
+        titleNamePanel.setBounds(boundX, boundY, boundWidth, boundHeight);
+        // setting the panel background
+        titleNamePanel.setBackground(titleNamePanelColor);
+
+        // adding a panel
+        container.add(titleNamePanel);
 
         // able to be set by admin control
         int startPotionAmount = 5;

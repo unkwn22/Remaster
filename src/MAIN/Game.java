6f07c48d071664bbf65c3932791197b31d6e92b5
unkwn <lsjc12911@naver.com>
2022-06-23 15:01:58 +0900

    created basic JFrame with properties

--- src/MAIN/Game.java
+++ src/MAIN/Game.java
@@ -3,10 +3,34 @@ package MAIN;
 import HANDLER.InitMain;
 import INIT.*;
 
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
 public class Game {
+
+    JFrame window;
+
+    // Frame properties
+    int windowWidth = 800;
+    int windowHeight = 600;
+    Color backGroundColor = Color.black;
+    boolean visibleSet = true;
+
     public Game(){
+
+        // creating a JFrame panel
+        window = new JFrame();
+        // setting the size of the panel
+        window.setSize(windowWidth, windowHeight);
+        // creating a JFrame close option, used to make the JFrame close properly
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // setting the frame background color
+        window.getContentPane().setBackground(backGroundColor);
+        // setting a the frame layout, setting it to null means turning off the default layout
+        window.setLayout(null);
+        // even though we created a window component, it isnt visible in the UI so to make it visible...
+        window.setVisible(visibleSet);
+
         // able to be set by admin control
         int startPotionAmount = 5;
 

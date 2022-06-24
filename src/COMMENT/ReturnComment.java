10a4fcbad2435caeebe64a499da890f5a71e4cae
unkwn <lsjc12911@naver.com>
2022-06-24 11:16:06 +0900

    added confirm box and validation method for username input

--- src/COMMENT/ReturnComment.java
+++
@@ -1,7 +0,0 @@
-package COMMENT;
-
-public class ReturnComment {
-
-    // Code: I No: 1
-    String fullInventory = "Not enough inventory space.";
-}
--- src/HANDLER/CharacterCreationHandler.java
+++ src/HANDLER/CharacterCreationHandler.java
@@ -2,11 +2,14 @@ package HANDLER;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CharacterCreationHandler {
+public class CharacterCreationHandler implements ActionListener{
 
     Container container;
 
+    // JPanel properties
     JPanel inputNamePanel;
     int inputNameBoundX       = 100;
     int inputNameBoundY       = 100;
@@ -14,11 +17,25 @@ public class CharacterCreationHandler {
     int inputNameBoundHeight  = 250;
     Color inputPanelBackColor = Color.blue;
 
-    JTextField inputTextField;
-    int inputTextBoundX      = 100;
-    int inputTextBoundY      = 100;
-    int inputTextBoundWidth  = 100;
-    int inputTextBoundHeight = 100;
+    // JTextArea properties
+    JTextArea textArea, inputTextField;
+    String askText          = "Type in your name.";
+    String validText        = "Valid name, Please type in your name again.";
+    Color textAreaBackColor = Color.black;
+    Color textAreaForeColor = Color.white;
+    boolean lineWrap        = true;
+
+    String titleFontName    = "Times new Roman";
+    int titleFontProperties = Font.PLAIN;
+    int normalFontSize      = 30;
+    Font normalFont         = new Font(titleFontName, titleFontProperties, normalFontSize);
+    boolean setDisable      = false;
+
+    // JButton properties
+    JButton confirmBtn;
+    Color confirmBtnBackColor  = Color.black;
+    Color confirmBtnForeColor  = Color.white;
+    String confirmBtnLabel     = "Confirm";
 
     // making a grid criteria for panel layout
     GridLayout gridLayout    = new GridLayout(3, 1);
@@ -31,6 +48,7 @@ public class CharacterCreationHandler {
 
     // creating character creation page
     public void createScreen(){
+        // input name panel
         inputNamePanel = new JPanel();
         inputNamePanel.setBounds(inputNameBoundX, inputNameBoundY, inputNameBoundWidth, inputNameBoundHeight);
         inputNamePanel.setBackground(inputPanelBackColor);
@@ -38,8 +56,33 @@ public class CharacterCreationHandler {
         inputNamePanel.setLayout(gridLayout);
         container.add(inputNamePanel);
 
-        inputTextField = new JTextField();
+        // textArea
+        textArea = new JTextArea(askText);
+        textArea.setBackground(textAreaBackColor);
+        textArea.setForeground(textAreaForeColor);
+        textArea.setFont(normalFont);
+        textArea.setLineWrap(lineWrap);
+        textArea.setEnabled(setDisable);
+        inputNamePanel.add(textArea);
+
+        // JTextField
+        inputTextField = new JTextArea();
         inputNamePanel.add(inputTextField);
+
+        // confirm button
+        confirmBtn = new JButton(confirmBtnLabel);
+        confirmBtn.setBackground(confirmBtnBackColor);
+        confirmBtn.setForeground(confirmBtnForeColor);
+        confirmBtn.setFont(normalFont);
+        // getting the inputTextField text when confirm button is clicked
+        confirmBtn.addActionListener(this);
+        inputNamePanel.add(confirmBtn);
     }
 
+    // name validation check
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // getting the name input from the user
+        String name = inputTextField.getText();
+    }
 }
